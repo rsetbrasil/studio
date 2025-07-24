f64ba01e340466de1792ff2e0c38119c5852576e
Rset Brasil Tecnologia <rsetbrasil@gmail.com>
2025-07-24 17:42:08 +0000

    esta aqui
    // Import the functions you need from the SDKs you need
    impo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyDjgftyrnCTFVlqIQB7Jpc6IgrzIWRSu28",
+  authDomain: "sipstream-ev33d.firebaseapp.com",
+  projectId: "sipstream-ev33d",
+  storageBucket: "sipstream-ev33d.appspot.com",
+  messagingSenderId: "1010935066783",
+  appId: "1:1010935066783:web:26305a990b4d6fb43c72aa"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
