d3c94e2c52c9054ab6953ba67ab81ce4f3eb3bbe
Rset Brasil Tecnologia <rsetbrasil@gmail.com>
2025-07-24 16:47:32 +0000

    pRONTO AGORA O BANCO FOI CRIADO

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -517,7 +517,7 @@ export default function ProductsPage() {
                         <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                         <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                         <TableCell><Skeleton className="h-5 w-32" /></TableCell>
-                        <TableCell><Skeleton className="h-9 w-48" /></TableCell>
+                        <TableCell><div className="flex gap-2"><Skeleton className="h-9 w-24" /><Skeleton className="h-9 w-24" /></div></TableCell>
                       </TableRow>
                     ))}
                   </>
