8196cb9e3c03f9f524fee1aa53acfff6eb0ca3b1
Rset Brasil Tecnologia <rsetbrasil@gmail.com>
2025-07-24 15:50:23 +0000

    Error: In HTML, <tr> cannot be a child of <td>. This will cause a hydrat

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -237,7 +237,22 @@ export default function ProductsPage() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {isMounted && products.length > 0 ? (
+                {!isMounted ? (
+                  <>
+                    {Array.from({ length: 5 }).map((_, i) => (
+                      <TableRow key={i}>
+                        <TableCell><Skeleton className="h-5 w-16" /></TableCell>
+                        <TableCell><Skeleton className="h-5 w-48" /></TableCell>
+                        <TableCell><Skeleton className="h-5 w-20" /></TableCell>
+                        <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+                        <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+                        <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+                        <TableCell><Skeleton className="h-5 w-32" /></TableCell>
+                        <TableCell><Skeleton className="h-9 w-48" /></TableCell>
+                      </TableRow>
+                    ))}
+                  </>
+                ) : filteredProducts.length > 0 ? (
                   filteredProducts.map((product) => (
                     <TableRow key={product.id}>
                       <TableCell className="font-medium">
@@ -272,22 +287,7 @@ export default function ProductsPage() {
                 ) : (
                   <TableRow>
                     <TableCell colSpan={8} className="h-24 text-center">
-                      {!isMounted ? (
-                        <>
-                          {Array.from({ length: 5 }).map((_, i) => (
-                            <TableRow key={i}>
-                              <TableCell><Skeleton className="h-5 w-16" /></TableCell>
-                              <TableCell><Skeleton className="h-5 w-48" /></TableCell>
-                              <TableCell><Skeleton className="h-5 w-20" /></TableCell>
-                              <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                              <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                              <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                              <TableCell><Skeleton className="h-5 w-32" /></TableCell>
-                              <TableCell><Skeleton className="h-9 w-48" /></TableCell>
-                            </TableRow>
-                          ))}
-                        </>
-                      ) : "Nenhum produto encontrado."}
+                      Nenhum produto encontrado.
                     </TableCell>
                   </TableRow>
                 )}
